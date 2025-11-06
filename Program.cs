bbb599c76b04a8687ea8d34560eb2521682b4a4c
m9henri <missolhenri@gmail.com>
2025-11-06 18:23:09 +0100

    fixed rollback + generated new bug

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace trackingapp
 {
     class Program
     {
-        public static string choice;
+        public static string? choice;
         class Entry
         {
             public int day, month, year;
@@ -16,17 +16,17 @@ namespace trackingapp
         public static void Main(string[] args)
         {
             Console.Clear();
-            start();
+            Start();
         }
 
-        static void start()
+        static void Start()
         {
-            Console.WriteLine("Heute oder anderes Datum?\n\n 1 heutiges Datum\n 2 anderes Datum\n");
+            Console.WriteLine("Heutiges oder anderes Datum?\n\n 1 heutiges Datum\n 2 anderes Datum\n");
             choice = Console.ReadLine();
             switch (choice)
             {
                 case "1":
-                    System.Environment.Exit(0); // temp fix
+                    System.Environment.Exit(2); // temp fix
                     break;
                 case "2":
                     CreateEntry();
@@ -42,31 +42,42 @@ namespace trackingapp
             string inputcsv;
             Entry newEntry = new();
 
-
-            Console.Write("\nJahr? ");
-            newEntry.year = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nTag? ");
+            newEntry.day = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Monat? ");
             newEntry.month = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Tag? ");
-            newEntry.day = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Jahr? ");
+            newEntry.year = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Wie viel Geld hast du ausgegeben? ");
+            Console.Write("Wie viel hast du ausgegeben? ");
             newEntry.amount = Convert.ToSingle(Console.ReadLine());
 
-            Console.Write($"Stimmt das? {newEntry.day}.{newEntry.month}.{newEntry.year}: {newEntry.amount} J/n ");
-            choice = Console.ReadLine();
+            Console.Write($"Ist das richtig? {newEntry.day}.{newEntry.month}.{newEntry.year}: {newEntry.amount} [J/n] ");
+            choice = Console.ReadLine(); // new line != null
             if (choice is not null || choice != "J")
             {
+                // Console.Clear();
+                // CreateEntry();
+                Console.WriteLine(choice);
+                System.Environment.Exit(-67);
+            }
+
+            if (newEntry.month > 12 || newEntry.day > 31 || newEntry.amount <= 0)
+            {
+                Console.WriteLine("Dein Datum oder der Betrag stimmt nicht.");
+                Task.Delay(2500);
+                Console.Clear();
                 CreateEntry();
             }
+            
             inputcsv = $"{newEntry.year},{newEntry.month},{newEntry.day},\"{newEntry.amount}\"";
             File.AppendAllText("entries.csv", $"\n{inputcsv}");
-            Console.WriteLine("Der Eintrag wurde erstellt.");
+            Console.WriteLine("Eintrag wurde erstellt.");
             Task.Delay(3000);
             Console.Clear();
-            start();
+            Start();
         }
     }
 }
